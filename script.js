7c46307008df41720cb5b880afeb9686ad52179c
ahmadziadev <ziamain@proton.me>
2026-07-25 22:06:27 +0500

    Handle screen text, number and control functions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function add(a, b) {
     return Number(a) + Number(b);
 }
@@ -17,6 +18,56 @@ function divide(a, b) {
 let firstOperand;
 let secondOperand;
 let operator;
+let numbers = document.querySelectorAll(".number");
+let screen = document.querySelector(".screen");
+let decimalBtn = document.querySelector("#decimal");
+numbers.forEach(number => number.addEventListener("click", e => {
+    if (screen.innerHTML.slice().replace(".", "").length < 12) {
+        appendScreen(number.innerHTML);
+    }
+}));
+
+function appendScreen(text) {
+    screen.innerHTML = removeLeadingZeros(screen.innerHTML + text);
+    checkDecimal();
+    return;
+}
+function checkDecimal() {
+    if (screen.innerHTML.includes(".")) {
+        decimalBtn.disabled = true;
+    }
+}
+function clearScreen() {
+    screen.innerHTML = "";
+    decimalBtn.disabled = false;
+}
+function removeLeadingZeros(text) {
+    text = text.replace(/^0+/, '0');
+    if (text.length > 1 && text[1] != '.' && text[0] == '0') return text.slice(1);
+    return text;
+}
+let controls = document.querySelectorAll(".controls");
+controls.forEach(control => {
+    control.addEventListener("click", () => {
+        switch (control.id) {
+            case "c":
+                firstOperand = undefined;
+                secondOperand = undefined;
+                operator = undefined;
+                break;
+            
+            case "equals":
+                secondOperand = screen.innerHTML;
+                operate(firstOperand, operator, secondOperand);
+                break;
+            
+        
+            default:
+                break;
+        }
+        clearScreen(); 
+    })
+})
 
 function operate(firstOperand, operator, secondOperand) {
     switch (operator) {
