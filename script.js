bc825e365afcc57693ff121715be54ad811167a6
ahmadziadev <ziamain@proton.me>
2026-07-25 11:24:19 +0500

    Create operate()

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(a, b) {
 function divide(a, b) {
     return Number(a) / Number(b);
 }
+
+let firstOperand;
+let secondOperand;
+let operator;
+
+function operate(firstOperand, operator, secondOperand) {
+    switch (operator) {
+        case "+":
+            return add(firstOperand, secondOperand);
+        case "-":
+            return subtract(firstOperand, secondOperand);
+        case "*":
+            return multiply(firstOperand, secondOperand);
+        case "/":
+            return divide(firstOperand, secondOperand);
+        default:
+            return null;
+    }
+}
